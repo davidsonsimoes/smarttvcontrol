e31f71a5374e39051c6a420f5e93d5addc785e4c
Gabriel Santos <gabriel.santos@users-MacBook-Pro.local>
2019-06-27 15:52:59 -0300

    Install Styled-components dependencies

--- src/pages/connect-to-tv/index.js
+++ src/pages/connect-to-tv/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import { View, Text } from 'react-native';
 import { Link } from "react-router-native";
+import styled from 'styled-components';
+
 
 
 
@@ -9,11 +11,26 @@ export default class ConnectToTv extends Component {
     return (
       <View>
           <Text>Connect To Tv Page</Text>
-          <Link to="/control" underlayColor="#f0f4f7">
-            <Text>Control</Text>
+          <Link to="/control">
+          <ConnectButton>
+
+          </ConnectButton>
           </Link>
       </View>
     );
   }
 }
 
+
+const ConnectButton = styled.View`
+  position: relative;
+  top: 50%;
+  left: 50%;
+  margin-left: -50px;
+  margin-top: -50px;
+  display: flex;
+  width: 100px;
+  height: 100px;
+  font-size: 1px;
+  background: #5a99d4;
+`;
