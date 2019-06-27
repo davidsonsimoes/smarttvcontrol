1baf200ab1352ec6adf7c5f30805277d5520a9b7
Vinicius Pontes <virtualmachine@MacBook-Pro-de-virtual-3.local>
2019-06-27 17:55:59 -0300

    Adicionado buttons

--- src/pages/control/index.js
+++ src/pages/control/index.js
@@ -1,13 +1,43 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { Link } from "react-router-native";
+
+//Styles
+import {
+  HeaderDiv,
+  HeaderText,
+  ButtonDiv,
+  UpAndDownDiv,
+  LeftAndRightDiv
+} from './styles'
 
 
 export default class Control extends Component {
+
+  renderHeader = () => (
+    <HeaderDiv>
+      <HeaderText>Você está no controle</HeaderText>
+    </HeaderDiv>
+  )
+
+  renderButtons = () => {
+    return (
+      <View>
+        <Button title="up" />
+        <Button title="down" />
+        <Button title="left" />
+        <Button title="right" />
+        <Button title="ok" />
+        
+      </View>
+    )
+  }
+
   render() {
     return (
       <View>
-          <Text>Control</Text>
+          {this.renderHeader()}
+          {this.renderButtons()}
           <Link to="/" underlayColor="#f0f4f7">
             <Text>Home</Text>
           </Link>
---
+++ src/pages/control/styles.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components' 
+
+export const HeaderDiv = styled.View`
+    height: 40px;
+    background-color: red;
+    display: flex;
+    justify-content: center;
+    
+    `
+
+export const HeaderText = styled.Text`
+    color: #fff;
+    font-size: 20px;
+    text-transform: uppercase;
+    font-weight: 400;
+    text-align: center;
+`
+
+// Buttons Styles
+
+export const ButtonDiv = styled.View`
+    display: flex;
+    flex-flow: row wrap;
+`
+
+export const UpAndDownDiv = styled.View`
+    flex: ;
+    text-align: center;
+`
+
+export const LeftAndRightDiv = styled.View`
+    flex:1;      
+    display: flex;
+`
