5ac7fa3fd62b2cb0d43399e9bd67c08d5c051683
Gabriel Santos <gabriel.santos@users-MacBook-Pro.local>
2019-06-27 14:18:32 -0300

    Configure router to the app

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
-import React, {Component} from 'react';
-import { View, SafeAreaView } from 'react-native';
-import Index from './src/index'
+import React, {Component} from 'react'
+import { View, SafeAreaView } from 'react-native'
+import { NativeRouter, Route } from 'react-router-native'
+
+import ConnectToTv from './src/pages/connect-to-tv'
+import Control from './src/pages/control'
 
 
 
 export default class App extends Component {
   render() {
     return (
-      <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
-        <View style={{flex: 1}}>
-          <Index />
-        </View>
-      </SafeAreaView>
+      <NativeRouter>
+        <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+          <View style={{flex: 1}}>
+            <Route exact path="/" component={ConnectToTv} />
+            <Route path="/control" component={Control} />
+          </View>
+        </SafeAreaView>
+      </NativeRouter>
     );
   }
 }
--- src/pages/connect-to-tv/index.js
+++ src/pages/connect-to-tv/index.js
@@ -3,13 +3,14 @@ import { View, Text } from 'react-native';
 import { Link } from "react-router-native";
 
 
-export default class Control extends Component {
+
+export default class ConnectToTv extends Component {
   render() {
     return (
       <View>
-          <Text>Control</Text>
-          <Link to="/" underlayColor="#f0f4f7">
-            <Text>Home</Text>
+          <Text>Connect To Tv Page</Text>
+          <Link to="/control" underlayColor="#f0f4f7">
+            <Text>Control</Text>
           </Link>
       </View>
     );
