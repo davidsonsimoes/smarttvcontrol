8330ea557a0c6abb4649fa1e3b18a535e87fa379
Gabriel Santos <gabriel.santos@users-MacBook-Pro.local>
2019-07-01 10:00:35 -0300

    Margin on buttons

--- src/pages/connect-to-tv/index.js
+++ src/pages/connect-to-tv/index.js
@@ -45,7 +45,7 @@ export default class ConnectToTv extends Component {
     }
 
     if (tvFound == '') {
-      buttonOrLabbel = <Text>Nenhuma TV encontrada.</Text>;
+      buttonOrLabbel = <Text style={{marginTop: 25, color: "grey"}}>Nenhuma TV encontrada.</Text>;
     } else {
       buttonOrLabbel = (
       <TouchableOpacity style={styles.connectButton} onPress={() => this.onConnect()}>
@@ -58,7 +58,7 @@ export default class ConnectToTv extends Component {
         <TouchableOpacity style={styles.findTvButton} title="Change" onPress={() => this.getTvData()}>
           <Pulse color='red' numPulses={3} diameter={200} speed={20} duration={1000} />
         </TouchableOpacity>
-        <Text>Toque para conectar a TV</Text> 
+        <Text style={{marginTop: 25}}>Toque para conectar a TV</Text> 
         {buttonOrLabbel}
       </View>
     );
